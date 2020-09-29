e6e0119169802fa38b343528d936f42bc2a79417
Lukáš Kramár <u19.414@pslib.cloud>
2020-09-29 08:15:30 +0200

    Prvnich 10 min. prace

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int PowerOfTwo(int parametr)
+        {
+            
+            return vysledek
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
