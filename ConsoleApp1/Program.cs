b38645ae28a759437054ad7432dfa275291ed09f
Lukáš Kramár <u19.414@pslib.cloud>
2020-09-29 08:32:00 +0200

    druha kontrola po 10 min. prace

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,13 +4,33 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static int PowerOfTwo(int parametr)
+        static double PowerOfTwo(double parametr)
         {
-            
-            return vysledek
+            double vysledek = Math.Pow(0, parametr);
+            return vysledek;
+        }
+
+        static string WriteStars(int cislo)
+        {
+            string vysledek = "";
+            for (int i = cislo; i >0; i = i-1)
+            {
+                vysledek += "*";
+            }
+            return vysledek;
         }
         static void Main(string[] args)
         {
+            int cislo;
+            bool zkouska;
+            int vysledek;
+            do
+            {
+                Console.Write("Zadejte prosím číslo od 3 do 15 včetně: ");
+                string vstup = Console.ReadLine();
+                zkouska = Int32.TryParse(vstup, out cislo);                
+            }
+            while ((zkouska) && (cislo > 3) && (cislo < 16));
             Console.WriteLine("Hello World!");
         }
     }
